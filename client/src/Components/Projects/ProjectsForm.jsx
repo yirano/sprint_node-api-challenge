ce4c3bbf123feefc163e3544bc825e01e6440f7c
yirano <yirano@protonmail.ch>
2020-07-20 15:45:47 -0400

    Remove 'e' from handleSubmit function

--- client/src/Components/Projects/ProjectsForm.jsx
+++ client/src/Components/Projects/ProjectsForm.jsx
@@ -16,7 +16,7 @@ const ProjectsForm = (props) => {
     setForm({ ...form, [e.target.name]: e.target.value })
   }
 
-  const handleSubmit = e => {
+  const handleSubmit = () => {
     postProject(form)
   }
 

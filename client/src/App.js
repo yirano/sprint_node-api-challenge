f52b106ec1ecd58724c279d6607a7fffb06b9f8b
yirano <yirano@protonmail.ch>
2020-07-19 22:56:10 -0400

    Add Route and Link

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import ProjectForm from './Components/Forms/ProjectForm'
 import ActionForm from './Components/Forms/ActionForm'
 import ProjectRender from './Components/Render/ProjectRender'
@@ -6,12 +7,18 @@ import ActionRender from './Components/Render/ActionRender'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Link to="/projects">Projects</Link>
+      <Link to="/actions">Actions</Link>
       <ProjectForm />
       <ActionForm />
-      <ProjectRender />
-      <ActionRender />
-    </div>
+      <Switch>
+        <Route path="/projects" component={ProjectRender} />
+        <Route path="/actions" component={ActionRender} />
+        {/* <ProjectRender /> */}
+        {/* <ActionRender /> */}
+      </Switch>
+    </Router>
   )
 }
 

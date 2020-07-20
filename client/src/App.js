90ae53d2d6384b09be14072ee414fc185be4d421
yirano <yirano@protonmail.ch>
2020-07-19 22:59:04 -0400

    Style button links

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,31 @@ import ProjectForm from './Components/Forms/ProjectForm'
 import ActionForm from './Components/Forms/ActionForm'
 import ProjectRender from './Components/Render/ProjectRender'
 import ActionRender from './Components/Render/ActionRender'
+import styled from 'styled-components'
+
+const StyledLink = styled.div`
+  a {
+    color: black;
+    padding: 10px 20px;
+    margin: 10px;
+    font-size: 20px;
+    text-decoration: none;
+    border: 1px solid blue;
+    border-radius: 10px;
+    &:hover {
+      background-color: black;
+      color: white;
+    }
+  }
+`
 
 function App() {
   return (
     <Router>
-      <Link to="/projects">Projects</Link>
-      <Link to="/actions">Actions</Link>
+      <StyledLink>
+        <Link to="/projects">Projects</Link>
+        <Link to="/actions">Actions</Link>
+      </StyledLink>
       <ProjectForm />
       <ActionForm />
       <Switch>
